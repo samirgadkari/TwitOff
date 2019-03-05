e37399970dd634f7c6c3dd8164fbacff9d0abe47
Samir Gadkari <samir.gadkari@gmail.com>
2019-03-05 13:49:13 -0800

    Twitter and Basilica interface

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,15 +1,25 @@
 """Main application and logic for TwitOff"""
-from flask import Flask
-from .models import DB
+from decouple import config
+from flask import Flask, render_template, request
+from .models import DB, User
 
 def create_app():
     """Create and configure and instance of the Flask application."""
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
+    app.config['ENV'] = config('ENV')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     DB.init_app(app)
 
     @app.route('/')
     def root():
-        return "Welcome to TwitOff!"
+        users = User.query.all()
+        return render_template('base.html', title='Home', users=users)
+
+    @app.route('/reset')
+    def reset():
+        DB.drop_all()
+        DB.create_all()
+        return render_template('base.html', title='DB Reset!', users=[])
 
     return app
--- twitoff/models.py
+++ twitoff/models.py
@@ -5,17 +5,22 @@ DB = SQLAlchemy()
 
 class User(DB.Model):
     """Twitter users that we pull and analyze tweets for."""
-    id = DB.Column(DB.Integer, primary_key=True)
+    id = DB.Column(DB.BigInteger, primary_key=True)
     name = DB.Column(DB.String(15), nullable=False)
+    newest_tweet_id = DB.Column(DB.BigInteger)
 
     def __repr__(self):
         return '<User {}>'.format(self.name)
 
 class Tweet(DB.Model):
     """Tweets"""
-    id = DB.Column(DB.Integer, primary_key=True)
-    text = DB.Column(DB.Unicode(280))
-    user_id = DB.Column(DB.Integer, DB.ForeignKey('user.id'), nullable=False)
+    id = DB.Column(DB.BigInteger, primary_key=True)
+    text = DB.Column(DB.Unicode(500)) # Tweets can be big, so you will have some truncated tweets
+
+    # Pickle is the package for serializing/deserializing things
+    embedding = DB.Column(DB.PickleType, nullable=False)
+
+    user_id = DB.Column(DB.BigInteger, DB.ForeignKey('user.id'), nullable=False)
     user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
 
     def __repr__(self):
